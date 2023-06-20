9ab08f54c8d1da4de6740908e79b1bb3984e16ef
B Bogdan <125175907+MrBogdanYT@users.noreply.github.com>
2023-06-20 18:56:13 +0300

    Create os.py

---
+++ os.py
@@ -0,0 +1,42 @@
+import os
+
+
+# Ex 1
+# dir = os.getcwd()
+# print(f"Current Directory is {dir}")
+
+# Ex 2
+# def current_path():
+#     print("Current Directory is " + os.getcwd())
+
+# current_path()
+# os.chdir('../')
+# current_path()
+
+
+# Ex 3
+# directory = "test"
+# parent_dir = r"\Users\Codeland 41\Desktop\Proiecte\p1"
+
+# path = os.path.join(parent_dir , directory)
+
+# os.mkdir(path)
+
+# Ex 4
+# path = '/'
+# dir_list = os.listdir(path)
+# print(dir_list)
+
+# Ex 5
+# file = 'test.txt'
+# location = r"\Users\Codeland 41\Desktop\Proiecte\p1"
+# path = os.path.join(location , file)
+
+# os.remove(path)
+
+# Ex 6
+# directory = 'test'
+# location = r"\Users\Codeland 41\Desktop\Proiecte\p1"
+# path = os.path.join(location , directory)
+
+# os.rmdir(path)
